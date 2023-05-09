1b6b7cc304968d6e88abed7034c92ae59e39a70c
Aria <tarapiccari@gmail.com>
2023-05-09 01:38:19 -0700

    Fix working directory bug in harbinger_update

--- server/bash_scripts/harbinger_update.sh
+++ server/bash_scripts/harbinger_update.sh
@@ -5,8 +5,9 @@ WHOAMI=$(id -unz)
 
 if [ $WHOAMI == "harbinger" ]
 then
+  cd "$1"
   echo "Please wait... Performing CI/CD update"
-  PSK="change_me"
+  PSK=$(<~/.harbinger_psk)
   PORT=7768
   echo "Moving Harbinger Update into place"
   mv build/libs/Harbinger-*.jar /harbinger/server.jar
@@ -19,5 +20,5 @@ then
   cp bash_scripts/pesc_harbinger_update_runner.sh /tmp/harbinger_updater2
   pesc_harbinger_update
 else
-  su -l harbinger -c "$(which harbinger_update)"
+  su -l harbinger -c "$(which harbinger_update) \"$(pwd)\""
 fi
