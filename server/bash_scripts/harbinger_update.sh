a708eac83d08a3581dc3535551062997b9bed0e9
Aria <tarapiccari@gmail.com>
2023-05-09 01:51:19 -0700

    Fix the server jar being previously unable to be moved into place during the update

--- server/bash_scripts/harbinger_update.sh
+++ server/bash_scripts/harbinger_update.sh
@@ -10,15 +10,14 @@ then
   PSK=$(<~/.harbinger_psk)
   PORT=7768
   echo "Moving Harbinger Update into place"
-  mv build/libs/Harbinger-*.jar /harbinger/server.jar
-  curl -d "{\"psk\":\"$PSK\"}" http://127.0.0.1:$PORT/stop
+  mv build/libs/Harbinger-*.jar /tmp/harbinger_server.jar
   echo "Allowing Harbinger to exit and be automatically restarted"
 
   echo "Updating service files..."
   cp bash_scripts/harbinger.service /tmp/harbinger_service_script
   cp bash_scripts/harbinger_update.sh /tmp/harbinger_updater
   cp bash_scripts/pesc_harbinger_update_runner.sh /tmp/harbinger_updater2
-  pesc_harbinger_update
+  pesc_harbinger_update $PORT "$PSK"
 else
   su -l harbinger -c "$(which harbinger_update) \"$(pwd)\""
 fi
--- server/bash_scripts/pesc_harbinger_update_runner.sh
+++ server/bash_scripts/pesc_harbinger_update_runner.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Do not execute this by itself!
-
+PORT=$1
+PSK=$2
 mv /tmp/harbinger_service_script /etc/systemd/system/harbinger.service
 mv /tmp/harbinger_updater /bin/harbinger_update
 mv /tmp/harbinger_updater2 /bin/pesc_harbinger_update_runner
+mv /tmp/harbinger_server.jar /harbinger/server.jar
+
+
+chown harbinger:harbinger /harbinger/server.jar
 
 chown root:root /bin/pesc_harbinger_update_runner
 chmod +x /bin/pesc_harbinger_update_runner
@@ -16,3 +21,4 @@ chown root:root /etc/systemd/system/harbinger.service
 
 systemd daemon-reload
 systemd reload harbinger
+curl -d "{\"psk\":\"$PSK\"}" http://127.0.0.1:$PORT/stop
