ebb05e2b4c19fba3935ebc55457e8520e5119d65
Aria <tarapiccari@gmail.com>
2023-05-09 18:23:46 -0700

    Add a /version endpoint

---
+++ server/src/main/java/dev/zontreck/harbinger/httphandlers/handlers/VersionCheckHandler.java
@@ -0,0 +1,23 @@
+package dev.zontreck.harbinger.httphandlers.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import dev.zontreck.harbinger.data.Persist;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class VersionCheckHandler implements HttpHandler
+{
+	@Override
+	public void handle(HttpExchange httpExchange) throws IOException {
+		String reply = "Version;;" + Persist.HARBINGER_VERSION;
+
+
+		byte[] bRep = reply.getBytes();
+		httpExchange.sendResponseHeaders(200, bRep.length);
+		OutputStream os = httpExchange.getResponseBody();
+		os.write(bRep);
+		os.close();
+	}
+}
