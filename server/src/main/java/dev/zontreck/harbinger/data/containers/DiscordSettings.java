c5839ce9ac8796d1428b2d6c20a29944949a6a28
Aria <tarapiccari@gmail.com>
2023-05-11 17:33:49 -0700

    Add the ability to register discord web hooks outside the scope of the bot

--- server/src/main/java/dev/zontreck/harbinger/data/containers/DiscordSettings.java
+++ server/src/main/java/dev/zontreck/harbinger/data/containers/DiscordSettings.java
@@ -4,7 +4,9 @@ import dev.zontreck.ariaslib.file.Entry;
 import dev.zontreck.ariaslib.file.EntryUtils;
 import dev.zontreck.ariaslib.file.Folder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Contains the settings for the Harbinger Discord Bot
@@ -12,11 +14,19 @@ import java.util.List;
 public class DiscordSettings
 {
 	public String BOT_TOKEN;
+	public Map<String, String> WEBHOOKS = new HashMap<>();
 
 	public Entry<List<Entry>> save()
 	{
 		Entry<List<Entry>> tag = Folder.getNew("discord");
 		tag.value.add(EntryUtils.mkStr("token", BOT_TOKEN));
+		Entry<List<Entry>> hooks = Folder.getNew("hooks");
+		for(Map.Entry<String,String> e : WEBHOOKS.entrySet())
+		{
+			hooks.value.add(EntryUtils.mkStr(e.getKey(), e.getValue()));
+		}
+		tag.value.add(hooks);
+
 
 		return tag;
 	}
@@ -27,5 +37,12 @@ public class DiscordSettings
 	public DiscordSettings(Entry<List<Entry>> tag)
 	{
 		BOT_TOKEN = EntryUtils.getStr(Folder.getEntry(tag, "token"));
+
+		Entry<List<Entry>> hooks = Folder.getEntry(tag, "hooks");
+		for(Entry<String> e : hooks.value)
+		{
+			WEBHOOKS.put(e.name, e.value);
+		}
+
 	}
 }
