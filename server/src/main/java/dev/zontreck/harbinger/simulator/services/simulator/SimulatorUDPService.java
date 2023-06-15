e38b49719f586302baad516a736962eccd311d2a
Aria <tarapiccari@gmail.com>
2023-06-14 23:52:14 -0700

    Add a handler to stop the udp server when the server is being torn down

--- server/src/main/java/dev/zontreck/harbinger/simulator/services/simulator/SimulatorUDPService.java
+++ server/src/main/java/dev/zontreck/harbinger/simulator/services/simulator/SimulatorUDPService.java
@@ -1,9 +1,10 @@
 package dev.zontreck.harbinger.simulator.services.simulator;
 
-import dev.zontreck.ariaslib.terminal.ConsolePrompt;
+import dev.zontreck.ariaslib.events.annotations.Subscribe;
 import dev.zontreck.ariaslib.terminal.Task;
 import dev.zontreck.ariaslib.util.DelayedExecutorService;
 import dev.zontreck.harbinger.data.Persist;
+import dev.zontreck.harbinger.events.ServerStoppingEvent;
 import dev.zontreck.harbinger.simulator.services.ServiceRegistry;
 
 import java.io.IOException;
@@ -12,21 +13,31 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
-public class SimulatorUDPService extends Task
-{
+public class SimulatorUDPService extends Task {
 	private static DatagramSocket socket;
 	private static boolean running;
 
+	@Subscribe
+	public static void onServerStopping ( ServerStoppingEvent ev ) {
+		if ( Persist.serverSettings.udp_settings.UDPServerEnabled ) {
+			if ( socket != null ) {
+				if ( socket.isBound ( ) )
+					socket.close ( );
+
+				socket = null;
+			}
+		}
+	}
+
 	public SimulatorUDPService ( String name ) throws SocketException {
-		super ( name, true );
+		super ( name , true );
 		socket = new DatagramSocket ( Persist.serverSettings.udp_settings.UDPPort );
 
 	}
 
-	public static void startService()
-	{
+	public static void startService ( ) {
 		try {
-			DelayedExecutorService.scheduleRepeatingTask ( new SimulatorUDPService ("UDP Server"), 1 );
+			DelayedExecutorService.scheduleRepeatingTask ( new SimulatorUDPService ( "UDP Server" ) , 1 );
 		} catch ( SocketException e ) {
 			throw new RuntimeException ( e );
 		}
@@ -34,23 +45,22 @@ public class SimulatorUDPService extends Task
 
 	@Override
 	public void run ( ) {
-		byte[] buffer = new byte[4096];
+		byte[] buffer = new byte[ 4096 ];
 
-		DatagramPacket packet = new DatagramPacket ( buffer, buffer.length );
+		DatagramPacket packet = new DatagramPacket ( buffer , buffer.length );
 		try {
 			socket.receive ( packet );
 
 
-
-			InetAddress ip = packet.getAddress ();
-			int port = packet.getPort ();
-			packet = new DatagramPacket ( buffer, buffer.length, ip, port );
-			String received = new String ( packet.getData (), 0, packet.getLength () );
+			InetAddress ip = packet.getAddress ( );
+			int port = packet.getPort ( );
+			packet = new DatagramPacket ( buffer , buffer.length , ip , port );
+			String received = new String ( packet.getData ( ) , 0 , packet.getLength ( ) );
 
 			ServiceRegistry.LOGGER.debug ( "UDP Packet data: " + received );
 
-			buffer = new byte[4096];
-			packet = new DatagramPacket ( buffer, buffer.length, ip, port );
+			buffer = new byte[ 4096 ];
+			packet = new DatagramPacket ( buffer , buffer.length , ip , port );
 
 			socket.send ( packet );
 		} catch ( IOException e ) {
