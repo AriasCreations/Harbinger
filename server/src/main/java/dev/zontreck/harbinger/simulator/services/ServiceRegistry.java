1c6f46461fa2eae8ea6740959e976a40b37b875d
Aria <tarapiccari@gmail.com>
2023-06-14 23:07:19 -0700

    Add in a simple and basic UDP server

--- server/src/main/java/dev/zontreck/harbinger/simulator/services/ServiceRegistry.java
+++ server/src/main/java/dev/zontreck/harbinger/simulator/services/ServiceRegistry.java
@@ -1,12 +1,19 @@
 package dev.zontreck.harbinger.simulator.services;
 
 import dev.zontreck.ariaslib.events.EventBus;
+import dev.zontreck.harbinger.commands.CommandRegistry;
 import dev.zontreck.harbinger.simulator.services.grid.PresenceService;
+import dev.zontreck.harbinger.simulator.services.simulator.SimulatorUDPService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServiceRegistry
 {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger ( ServiceRegistry.class.getSimpleName ( ) );
 	public static void register( EventBus bus )
 	{
 		bus.register ( PresenceService.class );
+		bus.register ( SimulatorUDPService.class );
 	}
 }
