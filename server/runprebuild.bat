a6234dde2dfe16f128e033be245c21bb07bd3b53
Zontreck <tarapiccari@gmail.com>
2023-08-10 01:55:17 -0700

    Add initial prebuild files

---
+++ server/runprebuild.bat
@@ -0,0 +1,3 @@
+@echo off
+
+dotnet Prebuild/bootstrap/prebuild.dll /target vs2022 /file Makefile.prebuild
---
+++ server/runprebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet Prebuild/bootstrap/prebuild.dll /target vs2022 /file Makefile.prebuild
