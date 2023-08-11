ac4d19cf1318adfe3715f3a8aa18defbaa02f983
Zontreck <tarapiccari@gmail.com>
2023-08-11 00:11:10 -0700

    Update prebuild to include SnapWrap

---
+++ server/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd Prebuild
+./compile.sh
+cd ..
+
+rm -rfv packaged || true
+mdir packaged
+rm -rf binaries/release
+mkdir binaries/release
+
+dotnet build -c Release -p:MyRuntimeIdentifier=linux-x64
+cd binaries/release
+tar -cvf ../../packaged/linux.tgz ./*
+
+cd ../..
+rm -rf binaries/release
+mkdir binaries/release
+
+dotnet build -c Release -p:MyRuntimeIdentifier=win-x64
+cd binaries/release
+tar -cvf ../../packaged/windows.tgz ./*
+
+cd ../..
+rm -rf binaries/release
+mkdir binaries/release
+
+dotnet build -c Release -p:MyRuntimeIdentifier=osx-x64
+cd binaries/release
+tar -cvf ../../packaged/osx.tgz ./*
