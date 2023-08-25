ed90468bf371dcb57fa7b9e055eace95133f2b49
Zontreck <tarapiccari@gmail.com>
2023-08-25 16:50:00 -0700

    Adds a toggle to output as json or raw value only

--- server/source/Utility/RegRead/Program.cs
+++ server/source/Utility/RegRead/Program.cs
@@ -10,16 +10,55 @@ namespace Harbinger.RegRead
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             string file = args[0];
             string key = args[1];
+            bool json = false;
+            if (args.Length > 2)
+            {
+                json = true;
+            }
             EventsBus.EventBus.debug = false;
 
             Key hive = RegistryIO.loadHive(file);
 
             Entry en = hive.getAtPath(key);
-            Console.WriteLine(JsonConvert.SerializeObject(en, Formatting.Indented));
+            if(json)
+                Console.WriteLine(JsonConvert.SerializeObject(en, Formatting.Indented));
+            else
+            {
+                switch (en.Type)
+                {
+                    case EntryType.Word:
+                        {
+                            Console.Write(en.Word().Value);
+                            break;
+                        }
+                    case EntryType.Int16:
+                        {
+                            Console.Write(en.Int16().Value); 
+                            break;
+                        }
+                    case EntryType.Int32:
+                        {
+                            Console.Write(en.Int32().Value);
+                            break;
+                        }
+                    case EntryType.Int64:
+                        {
+                            Console.Write(en.Int64().Value);
+                            break;
+                        }
+                    case EntryType.Bool:
+                        {
+                            Console.Write(en.Bool().Value);
+                            break;
+                        }
+                }
+            }
+
+            return (int) en.Type;
         }
     }
 }
