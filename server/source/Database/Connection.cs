9919109e5a4941084c2258d8ee690bfe55579bb8
Zontreck <tarapiccari@gmail.com>
2023-08-29 20:30:10 -0700

    Add a Database project

---
+++ server/source/Database/Connection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harbinger.Framework.Database
+{
+    public class Connection
+    {
+        internal string DatabaseName;
+        internal string Username;
+        internal string Password;
+
+        internal string Host;
+
+
+        public DbConnection connection;
+
+
+        public enum DatabaseType
+        {
+            SQLite,
+            MySQL
+        }
+
+
+        internal DatabaseType DbType;
+
+
+        internal Connection()
+        {
+
+        }
+
+
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+
+
+    public class ConnectionBuilder
+    {
+        public ConnectionBuilder()
+        {
+            connection = new Connection();
+        }
+
+        private Connection connection;
+
+        public ConnectionBuilder withDatabase(string file)
+        {
+            connection.DatabaseName = file;
+            return this;
+        }
+
+        public ConnectionBuilder withUserName(string userName)
+        {
+            connection.Username = userName;
+            return this;
+        }
+
+        public ConnectionBuilder withPassword(string password)
+        {
+            connection.Password = password;
+            return this;
+        }
+
+        public ConnectionBuilder withHost(string host)
+        {
+            connection.Host = host;
+            return this;
+        }
+
+        public ConnectionBuilder withDbType(Connection.DatabaseType databaseType)
+        {
+            connection.DbType = databaseType;
+            return this;
+        }
+
+        public Connection build()
+        {
+            if(connection.DbType == Connection.DatabaseType.SQLite)
+            {
+                connection.connection = new SQLiteConnection($"Data Source={connection.DatabaseName}; Version=3;");
+            } else
+            {
+                connection.connection = new SqlConnection($"Server={connection.Host}; Uid={connection.Username}; Pwd={connection.Password}; Database={connection.DatabaseName}");
+            }
+
+            connection.connection.Open();
+            
+            return connection;
+        }
+    }
+}
---
+++ server/source/Database/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SQLite.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.Common;
+
+namespace Harbinger.Framework.Database
+{
+    public class XTransaction
+    {
+        private Connection connection;
+        public XTransaction(Connection con)
+        {
+            connection = con;
+        }
+
+        public XTransaction(ConnectionBuilder builder)
+        {
+            connection = builder.build();
+
+        }
+
+        public DbTransaction GetTransaction()
+        {
+            return connection.connection.BeginTransaction();
+        }
+    }
+}
