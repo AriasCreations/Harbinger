d951e02c979edee876d00de68f858cf964248916
Zontreck <tarapiccari@gmail.com>
2023-09-24 04:33:20 -0700

    Remove duplicated scripts

--- clean.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-git clean -xfd
-git reset --hard
-
-git submodule foreach git clean -xfd
-git submodule foreach git reset --hard
--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clean -xfd
-git reset --hard
-
-git submodule foreach git clean -xfd
-git submodule foreach git reset --hard
