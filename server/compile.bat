85ecae554a7fdb4db593c88f47da5ccabaa74395
Zontreck <tarapiccari@gmail.com>
2023-10-07 22:22:03 -0700

    Generate manifest on compile

--- server/compile.bat
+++ server/compile.bat
@@ -6,3 +6,9 @@ cd ..
 
 call runprebuild.bat
 dotnet build -c Release
+
+cd binaries/updater/release
+dotnet Harbinger.Updater -genManifest -updaterMode -manifest ../../manifest.json -host https://ci.zontreck.dev/job/Harbinger%20Server/lastSuccessfulBuild/artifact
+cd ../..
+cd release
+dotnet Harbinger.Updater -genManifest -manifest ../manifest.json -host https://ci.zontreck.dev/job/Harbinger%20Server/lastSuccessfulBuild/artifact
