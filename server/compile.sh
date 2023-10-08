2b415d6471d3f9aa779510fbaa72270800217b34
Zontreck <tarapiccari@gmail.com>
2023-10-07 23:03:09 -0700

    Update the updater

--- server/compile.sh
+++ server/compile.sh
@@ -12,9 +12,9 @@ mkdir win
 mkdir linux
 mkdir mac
 
-HOST="https://ci.zontreck.dev/jobs/Harbinger Server/lastSuccessfulBuild/artifact"
+HOST="https://ci.zontreck.dev/jobs/Harbinger Server/lastSuccessfulBuild/artifact/server"
 
-
+dotnet Prebuild/bootstrap/prebuild.dll /target vs2022 /file Makefile.prebuild /rid linux-x64
 dotnet build -c Release -p:MyRuntimeIdentifier=linux-x64
 cd binaries/updater/release
 dotnet Harbinger.Updater.dll -plat linux -updaterMode -manifest ../../manifest.json -host $HOST -genManifest
@@ -26,6 +26,7 @@ cp binaries/manifest.json linux/
 
 rm -rf binaries
 
+dotnet Prebuild/bootstrap/prebuild.dll /target vs2022 /file Makefile.prebuild /rid win-x64
 dotnet build -c Release -p:MyRuntimeIdentifier=win-x64
 cd binaries/updater/release
 dotnet Harbinger.Updater.dll -plat win -updaterMode -manifest ../../manifest.json -host $HOST -genManifest
@@ -37,6 +38,7 @@ cp binaries/manifest.json win/
 
 rm -rf binaries
 
+dotnet Prebuild/bootstrap/prebuild.dll /target vs2022 /file Makefile.prebuild /rid osx-x64
 dotnet build -c Release -p:MyRuntimeIdentifier=osx-x64
 cd binaries/updater/release
 dotnet Harbinger.Updater.dll -plat mac -updaterMode -manifest ../../manifest.json -host $HOST -genManifest
